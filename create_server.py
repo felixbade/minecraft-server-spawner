718d56ad791850996cb8666ab7fd493bede3e7d4
Felix Bade <flix.bade@gmail.com>
2021-12-06 21:26:36 +0200

    Add create_server

---
+++ create_server.py
@@ -0,0 +1,31 @@
+from random import random
+
+import os
+
+from dotenv import load_dotenv
+
+from digitalocean import create_droplet
+
+load_dotenv()
+
+ssh_key = os.getenv('SSH_KEY_FINGERPRINT')
+
+hostname = 'minecraft'
+region = 'fra1' # Frankfurt 1
+size = 'c-2' # CPU-Optimized
+image = 'ubuntu-20-04-x64'
+ssh_keys = [ssh_key]
+user_data = open('user_data.txt').read()
+
+response = create_droplet(
+        name=hostname,
+        region=region,
+        size=size,
+        image=image,
+        user_data=user_data,
+        ssh_keys=ssh_keys,
+        volumes=None,
+        )
+
+from pprint import pprint
+pprint(response)
--- digitalocean.py
+++ digitalocean.py
@@ -9,7 +9,7 @@ load_dotenv()
 api_token = os.getenv('DIGITALOCEAN_TOKEN')
 
 def api_get(path):
-    url = 'https://api.digitalocean.com/v2/{}'.format(path)
+    url = 'https://api.digitalocean.com/v2{}'.format(path)
     headers = {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer {}'.format(api_token)
@@ -17,7 +17,32 @@ def api_get(path):
     response = requests.get(url, headers=headers)
     return json.loads(response.text)
 
+def api_post(path, body):
+    url = 'https://api.digitalocean.com/v2{}'.format(path)
+    headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {}'.format(api_token)
+            }
+    response = requests.post(url, json=body, headers=headers)
+    return json.loads(response.text)
+
+
+
 def get_droplets(page=1, per_page=10):
     path = '/droplets?page={}&per_page={}'.format(page, per_page)
     response = api_get(path)
     return response['droplets']
+
+def create_droplet(name, region, size, image, **kwargs):
+    # https://docs.digitalocean.com/reference/api/api-reference/#operation/create_droplet
+    path = '/droplets'
+    body = {
+        'name': name,
+        'region': region,
+        'size': size,
+        'image': image
+    }
+    body.update(kwargs) # ssh_keys, tags, user_data, etc.
+
+    response = api_post(path, body=body)
+    return response
